b420886c7e0315139bc10e955c99c9c022e942cb
Vinz <abc@xyz.com>
2024-08-11 22:18:22 +0100

    Day11

--- scripts/11-Todo-list.js
+++ scripts/11-Todo-list.js
@@ -16,20 +16,23 @@ function renderTodoList() {
 
  for (let i = 0; i < todoList.length; i++) {
   const todoObject = (todoList[i]);
-  const name = todoObject.name;
-  const dueDate = todoObject.dueDate;
+  const { name, dueDate } = todoObject;
+  // const dueDate = todoObject.dueDate;
+
+
   const html = `
-  <p>
-  ${name} ${dueDate}
+ <div>
+  ${name} </div> 
+  <div>${dueDate} </div>
   <button onclick="
   todoList.splice(${i}, 1);
   renderTodoList()
-  ">Delete</button>
-  </p>`;
+  " class = "delete-button">Delete</button>
+  `;
   todoListHTML += html;
 
  }
- console.log(todoListHTML);
+
 
  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 
@@ -39,8 +42,16 @@ function addTodo() {
  const inputElement = document.querySelector('.js-name-input');
  const name = inputElement.value;
 
- todoList.push(name);
- console.log(todoList);
+ const dateInputElement = document.querySelector('.js-due-date-input');
+ const dueDate = dateInputElement.value;
+
+ todoList.push({
+  // name: name,
+  // dueDate: dueDate,
+  name,
+  dueDate
+ });
+
 
  inputElement.value = '';
 
